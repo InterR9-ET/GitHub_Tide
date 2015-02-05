6d06c253fc163e94b4eafc9d7f4491d593c7274e
Administrator <Administrator@PC-20140724IGXH>
2015-02-05 10:26:56 +0800

    Merge origin/master
    Conflicts:
    	GitHub_Tide/build/built-jar.properties

---
+++ GitHub_Tide/src/main/run_xn_atm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Administrator
+ */
+public class run_xn_atm {
+    public static void main(String[] args) {
+        z.xn_atm.main Main = new z.xn_atm.main();
+        try {
+            Main.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
