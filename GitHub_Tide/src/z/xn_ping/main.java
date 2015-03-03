eb92f9937cca3f010fea88c5711e60af98673ba9
Administrator <Administrator@PC-20140724IGXH>
2015-03-03 15:37:23 +0800

    Merge origin/master
    Conflicts:
    	GitHub_Tide/src/util/Test/socket.java

--- GitHub_Tide/src/z/xn_ping/main.java
+++ GitHub_Tide/src/z/xn_ping/main.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-
-
 /**
  *
  * @author yangzhen
@@ -17,20 +15,18 @@ public class main extends Thread {
 
     //-------------------------------------日志---------------------------------------//
     private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(main.class.getName()); //获得logger
+
     static {
         org.apache.log4j.PropertyConfigurator.configureAndWatch("conf/log4j_xnping.config");
     }
     private static util.GetTools.tools tools = new util.GetTools.tools();//声明工具类
     //-------------------------------------日志---------------------------------------//
 
-    private static util.GetSql.csnms _csnms = new util.GetSql.csnms(); 
-    private static util.GetThread.thread _thread = new util.GetThread.thread(2);
-
-  
+    private static util.GetSql.csnms _csnms = new util.GetSql.csnms();
 
     public void run() {
         //加载数据库
-        if (db.ini(log,_csnms)) {
+        if (db.ini(log, _csnms)) {
             while (true) {
                 try {
                     doing_main();
@@ -43,35 +39,42 @@ public class main extends Thread {
     }
 
     public void doing_main() throws IOException {
-       File file1 = new File("pingper/");//-------------------------------------------------------------
-       File file2 = new File("pingper/dcn/");//                                 获
-       File file3 = new File("pingper/wx/");//                                  
-       File file4 = new File("pingper/lyg/");//                                 取
-       File file5 = new File("pingper/jyw/");//                                 
-       File file6 = new File("pingper/sz/");//                                 
-       fun f=new fun();                     //                                  件
-       List file=f.huoqwuwenjian(file1);//                             
-       List file22=f.huoqwuwenjian(file2);//                                    文
-       List file33=f.huoqwuwenjian(file3);//                                 
-       List file44=f.huoqwuwenjian(file4);//                                    
-       List file55=f.huoqwuwenjian(file5);//                                 
-       List file66=f.huoqwuwenjian(file6);//                                 
-      if(file22.size()>0){
-          file.add(file22);
-      } if(file33.size()>0){
-          file.add(file33 );// 
-      }  
-      if(file44.size()>0){
-       file.add(file44);//
-      }            
-      if(file55.size()>0){
-       file.add(file55);//  
-      }             
-      if(file66.size()>0){
-       file.add(file66);//--------------------------------------------------------------------------------
-      }
-       //传入文件名，处理文件
-       f.chulwj(log, file, _csnms);
+        File file1 = new File("pingper//");//-------------------------------------------------------------
+        File file2 = new File("pingper//dcn//");//                                 获
+        File file3 = new File("pingper//wx//");//                                  
+        File file4 = new File("pingper//lyg//");//                                 取
+        File file5 = new File("pingper//jyw//");//                                 
+        File file6 = new File("pingper//sz//");//                                 
+        fun f = new fun();                     //                                  件
+        List file = f.huoqwuwenjian(file1);//                             
+        List file22 = f.huoqwuwenjian(file2);//                                    文
+        List file33 = f.huoqwuwenjian(file3);//                                 
+        List file44 = f.huoqwuwenjian(file4);//                                    
+        List file55 = f.huoqwuwenjian(file5);//                                 
+        List file66 = f.huoqwuwenjian(file6);//                                 
+        if (file22.size() > 0) {
+            file.addAll(file22);
+        }
+        if (file33.size() > 0) {
+            file.addAll(file33);// 
+        }
+        if (file44.size() > 0) {
+            file.addAll(file44);//
+        }
+        if (file55.size() > 0) {
+            file.addAll(file55);//  
+        }
+        if (file66.size() > 0) {
+            file.addAll(file66);//--------------------------------------------------------------------------------
+        }
+        //传入文件名，处理文件
+        if (file.size() > 0) {
+            int F = file.size();
+            System.out.println(F + "+++" + file.get(0));
+            f.chulwj(log, file, _csnms);
+        } else {
+            log.info("无文件");
+        }
     }
 
 }
