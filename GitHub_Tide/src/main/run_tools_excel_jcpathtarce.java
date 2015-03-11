952b299cc78d71a223062aab0d50056edb49a0f1
zhen <zhen@WIN-32A0L35GGSA>
2015-03-11 15:04:28 +0800

    Merge origin/master
    Conflicts:
    	GitHub_Tide/src/z/xn_jason/db.java
    	GitHub_Tide/src/z/xn_jason/fun.java
    	GitHub_Tide/src/z/xn_jason/main.java

---
+++ GitHub_Tide/src/main/run_tools_excel_jcpathtarce.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Administrator
+ *
+ * @author excel中 a列 z列 数据作为电路编号，
+ * @author 验证a列 z列的数据 在系统中是否有电路，是否有路由
+ *
+ */
+public class run_tools_excel_jcpathtarce {
+
+    public static void main(String[] args) {
+        z.excel_jcpathtrace.main main = new z.excel_jcpathtrace.main();
+        try {
+            main.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
