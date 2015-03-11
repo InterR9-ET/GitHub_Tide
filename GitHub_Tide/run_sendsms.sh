9c8e03fb199ab406aeca3c4da511ad95c5157c48
zhen <zhen@WIN-32A0L35GGSA>
2015-03-11 14:46:17 +0800

    Merge origin/master
    Conflicts:
    	GitHub_Tide/src/z/xn_jason/db.java
    	GitHub_Tide/src/z/xn_jason/fun.java
    	GitHub_Tide/src/z/xn_jason/main.java

---
+++ GitHub_Tide/run_sendsms.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+LANG=zh_CN
+export LANG
+
+pross=main.run_sendsms
+date=`ps -ef |grep $pross|grep -v "grep" |awk '{print $2}'`
+echo now is $date
+
+if [ -z $date ]
+then
+echo '$pross is NULL PRO'
+else
+kill -9 $date
+echo kill -9 $date    $pross    OK
+fi
+
+
+#JAVA_HOME=/home/ljg/rejdk/jdk1.6.0_23
+#JAVA_VM=-hotspot
+
+LIBRARY="."
+for i in ./lib/*.jar; do
+LIBRARY="$LIBRARY":"$i"
+done
+
+PRO=./GitHub_Tide.jar
+CLASSPATH=$JAVA_HOME\lib\tools.jar:$JAVA_HOME\lib\dt.jar:$LIBRARY:$PRO
+MEM_ARGS="-Xms128m -Xmx256m"
+java $JAVA_VM $MEM_ARGS -cp $CLASSPATH main.run_sendsms
